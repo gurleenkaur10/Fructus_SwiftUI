11b86164e18388153e1b0d11c5c78f2562fc36f2
Gurleen Kaur <gurleen.kaur101010@gmail.com>
2023-04-29 19:13:40 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Gurleen Kaur on 2023-04-29.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
