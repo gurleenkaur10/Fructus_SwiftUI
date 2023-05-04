9742825d3a77a79223241510f05044e12718c35a
Gurleen Kaur <gurleen.kaur101010@gmail.com>
2023-05-03 21:05:59 -0400

    Created card view for each fruit, created onboarding screen for card layout. Also added mannual data file for fruit data.

---
+++ Fructus/App/OnboardingView.swift
@@ -0,0 +1,37 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by Gurleen Kaur on 2023-05-03.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    
+    //MARK: - Properties
+    
+    var fruits: [Fruit] = fruitData
+    
+    //MARK: - Body
+    
+    var body: some View {
+        TabView {
+            ForEach(fruits[0..<5]){ item in
+                FruitCardView(fruit: item)
+
+            }
+            
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+
+//MARK: - Preview
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView(fruits: fruitData)
+    }
+}
---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,42 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Gurleen Kaur on 2023-04-29.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    
+    //MARK: - Properties
+    
+    //MARK: - Body
+    var body: some View {
+        Button(action: {
+            print("Exit")
+        }) {
+            HStack(spacing: 8) {
+                Text("Start")
+                
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(
+                Capsule().strokeBorder(.white, lineWidth: 1.25)
+            )
+        }//: Button
+        .tint(.white)
+        
+    }
+}
+    //MARK: - Preview
+
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .previewLayout(.sizeThatFits)
+    }
+}
