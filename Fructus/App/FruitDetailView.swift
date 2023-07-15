a204a1b9650b5f9b0c87ea26136198d375d55328
Gurleen Kaur <gurleen.kaur101010@gmail.com>
2023-07-15 06:24:35 -0400

    Added Fruit details page with animation on the image. Also added the external link to open in default browser

---
+++ Fructus/App/FruitDetailView.swift
@@ -0,0 +1,64 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Gurleen Kaur on 2023-07-15.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    //MARK: - Properties
+    var fruit: Fruit
+    
+    //MARK: - Body
+    var body: some View {
+        NavigationView{
+            ScrollView(.vertical, showsIndicators: false){
+                VStack(alignment: .center, spacing: 20){
+                    //Header
+                    FruitHeaderView(fruit: fruit)
+                    
+                    VStack(alignment: .leading, spacing: 20){
+                        //Title
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        // Headline
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        //Nutrients
+                        
+                        //subheading
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        //description
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        //link
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
+                   
+                    }//: vstack
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                }//: vstack
+                .navigationBarTitle(fruit.title, displayMode: .inline)
+                .navigationBarHidden(true)
+            }//: scroll
+            .edgesIgnoringSafeArea(.top)
+        }//: Navigation
+    }
+}
+//MARK: - Preview
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView(fruit: fruitData[0])
+    }
+}
